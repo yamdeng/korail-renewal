424d23c2205f8f7f8056cd9abdfa37d18ef225fc
yamdeng <yamdeng@gmail.com>
2018-08-28 11:44:31 +0900

    프론트 요청에 따른 json 파일 연동

---
+++ src/config/constant.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const Constant = {};
+
+/*
+
+    request value
+
+    '메인_처음' : '메인_처음',
+    '교통_즐겨찾기' : '교통_즐겨찾기',
+    '교통_경로_검색' : '교통_경로_검색',
+    '교통_시작' : '교통_시작',
+    '교통_처음' : '교통_처음',
+    '기차_여정_선택' : '기차_여정_선택',
+
+*/
+
+Constant.REQUEST_VALUE_MAIN_FIRST = '메인_처음';
+Constant.REQUEST_VALUE_FAVORITE = '교통_즐겨찾기';
+Constant.REQUEST_VALUE_TRAFFIC_SEARCH = '교통_경로_검색';
+Constant.REQUEST_VALUE_TRAFFIC_START = '교통_시작';
+Constant.REQUEST_VALUE_TRAFFIC_FIRST = '교통_처음';
+Constant.REQUEST_VALUE_TRAIN_JOURNEY_SELECT = '교통_처음';
+
+module.exports = Constant;
