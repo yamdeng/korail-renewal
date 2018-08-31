5adbd072ec8fd9d82bd99c0058e39e00241479ab
yamdeng <yamdeng@gmail.com>
2018-08-31 12:07:46 +0900

    json 정보 수정

--- src/routes/korail.js
+++ src/routes/korail.js
@@ -5,18 +5,14 @@ const fs = require('fs');
 // const logger = require('../utils/logger');
 
 router.post('/requestMessage', function(req, res) {
-
-    // type : postback
-    // value : value 값에 따라 핸들링하는 구조인득
-    // request : 추가 파라미터
-
-    // const type = req.body.type;
-    const value = req.body.message.template.query;
-    const jsonFileName = value;
-    let jsonString = fs.readFileSync(__dirname + '/json/' + jsonFileName + '.json', 'utf8');
+    const query = req.body.message.template.query;
+    const jsonFileName = query;
+    let jsonString = fs.readFileSync(
+        __dirname + '/json/' + jsonFileName + '.json',
+        'utf8'
+    );
     let result = JSON.parse(jsonString);
     res.send(result);
-    
 });
 
 module.exports = router;
